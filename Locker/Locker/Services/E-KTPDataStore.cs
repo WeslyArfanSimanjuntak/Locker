a5a1cd8486f1f6cbc2c59cc7c17e7b9e09eea4a3
wesly arfan simanjuntak <weslyarfan@outlook.com>
2020-02-06 18:56:49 +0700

    Add project files.

---
+++ Locker/Locker/Services/E-KTPDataStore.cs
@@ -0,0 +1,40 @@
+﻿using Locker.Models.E_KTP;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace Locker.Services
+{
+   public class E_KTPDataStore
+    {
+        HttpClient client;
+        Content items;
+        private static string BackEndUrl = "http://ajk-aga.indosuryalife.co.id/hlt/EKTPApi/ceknik";
+        public E_KTPDataStore()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri($"{BackEndUrl}/");
+
+            items = new Content();
+        }
+
+        bool IsConnected => Connectivity.NetworkAccess == NetworkAccess.Internet;
+        public async Task<Content> GetItemsAsync(string nik, bool forceRefresh = true)
+        {
+            if (forceRefresh && IsConnected)
+            {
+                var json = await client.GetStringAsync(nik);
+                var rootObject = await Task.Run(() => JsonConvert.DeserializeObject<RootObject>(json));
+                items = rootObject.content.FirstOrDefault();
+            }
+
+            return items;
+        }
+
+    }
+}
---
+++ Locker/Locker/Views/E-KTP.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Locker.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Locker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class E_KTP : ContentPage
+    {
+        E_KTPDataStore e_ktpDataStore = new E_KTPDataStore();
+        public E_KTP()
+        {
+            InitializeComponent();
+        }
+        async void Search_Clicked(object sender, EventArgs e)
+        {
+            var nik = this.entryNIK.Text;
+            var searchingResult = await e_ktpDataStore.GetItemsAsync(nik, true);
+            this.labelFullName.Text = searchingResult.NAMA_LGKP;
+            this.labelNO_KK.Text = searchingResult.NO_KK.ToString();
+            this.labelNIK.Text = searchingResult.NIK.ToString();
+            this.labelKAB_NAME.Text = searchingResult.KAB_NAME;
+            this.labelNO_RW.Text = searchingResult.NO_RW?.ToString();
+            this.labelKEC_NAME.Text = searchingResult.KEC_NAME;
+            this.labelJENIS_PKRJN.Text = searchingResult.JENIS_PKRJN;
+            this.labelNO_RT.Text = searchingResult.NO_RT?.ToString();
+            this.labelNO_KEL.Text = searchingResult.NO_KEL?.ToString();
+            this.labelALAMAT.Text = searchingResult.ALAMAT;
+            this.labelNO_KEC.Text = searchingResult.NO_KEC?.ToString();
+            this.labelTMPT_LHR.Text = searchingResult.TMPT_LHR;
+            this.labelSTATUS_KAWIN.Text = searchingResult.STATUS_KAWIN;
+            this.labelNO_PROP.Text = searchingResult.NO_PROP?.ToString();
+            this.labelNAMA_LGKP_IBU.Text = searchingResult.NAMA_LGKP_IBU;
+            this.labelPROP_NAME.Text = searchingResult.PROP_NAME;
+            this.labelNO_KAB.Text = searchingResult.KAB_NAME;
+            this.labelKEL_NAME.Text = searchingResult.KEL_NAME;
+            this.labelJENIS_KLMIN.Text = searchingResult.JENIS_KLMIN;
+            this.labelTGL_LHR.Text = searchingResult.TGL_LHR;
+        }
+        void Clear_Clicked(object sender, EventArgs e)
+        {
+            this.entryNIK.Text = "";
+        }
+    }
+}
